5741f3c1c70b506e8d9a575284955ccf3d40c367
kaiwen <38695438+kaiwenlau@users.noreply.github.com>
2021-12-23 03:50:04 +0800

    setup prettier config and app base setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      <Button variant="contained">Hello World</Button>
-    </div>
+    <Container
+      maxWidth="sm"
+      sx={{
+        backgroundColor: 'primary.dark',
+      }}
+    >
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App v5 example
+        </Typography>
+      </Box>
+    </Container>
   );
 };
 
